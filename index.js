c9a2eaa02d3f11c851fb75914dad6df5b28309c8
iLuckYz1998 <lucasantls19@gmail.com>
2026-03-16 20:50:41 -0300

    ds

--- index.js
+++ index.js
@@ -7,3 +7,19 @@ const sequelize = new Sequelize('db_projeto', 'root','',{
     host: 'localhost',
         dialect: 'mysql'
 })
+
+//Definição de tabelas (modelo) de clientes
+const Cliente = sequelize.define ('Cliente', {
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    telefone: {
+        type: DataTypes.STRING,
+    }
+})
